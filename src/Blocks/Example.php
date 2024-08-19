026579de92117556fa34f3390f2e48abc01b17cd
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2024-08-19 22:33:22 +0200

    Add some example data to example block

--- src/Blocks/Example.php
+++ src/Blocks/Example.php
@@ -2,9 +2,27 @@
 
 namespace Jeffreyvr\Paver\Blocks;
 
+use Jeffreyvr\Paver\Blocks\Options\Input;
+
 class Example extends Block
 {
     public string $name = 'Example';
 
-    public array $children = [];
+    public array $data = [
+        'name' => '[your name here]'
+    ];
+
+    public function render()
+    {
+        return <<<HTML
+            <div style="text-align: center; padding: 25px;">This is an example block, {$this->data['name']} 👋</div>
+        HTML;
+    }
+
+    public function options()
+    {
+        return [
+            Input::make('Name', 'name')
+        ];
+    }
 }
